05f454326c8308f9591d23b0a2f0bccf964de00c
tmuzaffa <35147925+tmuzaffa@users.noreply.github.com>
2018-01-07 16:31:45 -0800

    Add files via upload

---
+++ p3.R
@@ -0,0 +1,8 @@
+with(dat, {
+  plot(Sub_metering_1 ~ Datetime, type = "l", 
+       ylab = "Global Active Power (kilowatts)", xlab = "")
+  lines(Sub_metering_2 ~ Datetime, col = 'Red')
+  lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+})
+dev.copy(png, file = "Plot3.png")
+dev.off()
